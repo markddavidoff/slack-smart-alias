2e61a4e1093fd9e296b8cd3a703e8ca8c6871e7a
Julian Ong <julian@tulipcremation.com>
2019-03-11 15:17:31 -0700

    Add Google Cal event creation, move params to SSM

--- set_alias.py
+++ set_alias.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python
 # Adapted from https://gist.github.com/devdazed/473ab227c323fb01838f
+from __future__ import print_function
+import boto3
+import dateutil.tz
 import json
 import logging
-from datetime import datetime
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
-import dateutil.tz
+import math
+import requests
 
-import boto3
-
+from botocore.exceptions import ClientError
+from datetime import datetime, timedelta
+from google.oauth2 import service_account
+from googleapiclient import discovery
 
 import settings
 
 # todo set this up with serverless
 log = logging.getLogger(__name__)
 
+KMS = boto3.client('kms')
+SSM = boto3.client('ssm')
+
 
 class SlackOnCall(object):
 
-    def __init__(self,
-                 slack_token,
+    def __init__(self, slack_token,
                  slack_user_group_handle,
-                 log_level='INFO'):
-       
+                 arn,
+                 log_level='INFO',
+                 keyfile=None,
+                 gcal_id=None,
+                 days_ahead=45):
+
         self.slack_token = slack_token
         self.slack_user_group_handle = slack_user_group_handle
 
         self._slack_user_group = None
-        self._on_call_email_addresses = None
         self._all_slack_users = None
+        self.google_keyfile = json.loads(keyfile)
+        self.google_calendar_id = gcal_id
+        self.arn = arn
+        self.days_ahead = int(days_ahead)
 
         log.setLevel(log_level)
 
@@ -38,38 +50,90 @@ class SlackOnCall(object):
         on-call user group in slack to be the on-call users
         """
 
-        slack_users = self.slack_users_by_email(self.on_call_email_addresses)
+        today = datetime.now(dateutil.tz.gettz(settings.TIMEZONE)).replace(hour=0, minute=0)
+
+        weekday = today.weekday()
+        oncalls = self.get_on_call_engs(weekday)
+        slack_users = self.slack_users_by_email(oncalls)
         if not slack_users:
-            log.warning('No Slack users found for email addresses: %s', ','.join(self.on_call_email_addresses))
+            log.warning('No Slack users found for email addresses: {0}'.format(','.join(oncalls)))
             return
 
+        # self.backfill_calendar_events(today, settings.DAYS_AHEAD)
+
+        if settings.GOOGLE_CAL_ID:
+            # set on-call calendar event for the future
+            the_future = today + timedelta(days=self.days_ahead)
+            weekday = the_future.weekday()
+            oncalls = self.get_on_call_engs(weekday)
+            self.set_calendar_event(oncalls, the_future)
+
+        # update slack oncall
         slack_user_ids = [u['id'] for u in slack_users]
-
         if set(slack_user_ids) == set(self.slack_user_group['users']):
-            log.info('User group %s already set to %s', self.slack_user_group_handle, slack_user_ids)
+            log.info(f'User group {self.slack_user_group_handle} already set to {slack_user_ids}')
             return
-
         self.update_on_call(slack_users)
+
         log.info('Job Complete')
 
-    def _make_request(self, url, body=None, headers={}):
-        #  TODO: now that we have serverless we can re-write this archaic dodo with requests
-        headers['Authorization'] = f'Bearer {self.slack_token}'
-        req = Request(url, body, headers)
-        log.info('Making request to %s', url)
+    def _make_request(self, url):
+        response = requests.get(url, headers={'Authorization': f'Bearer {self.slack_token}'})
+        return response.json()
 
-        try:
-            response = urlopen(req)
-            body = response.read()
-            try:
-                return json.loads(body)
-            except ValueError:
-                return body
+    def backfill_calendar_events(self, start_date, num_days):
+        """
+        Do not run unless you are prepared for calendar spam. Do not start on a sunday since this assumes Saturday
+        would have made Sunday's.  You will also need to increase the timeout to over a minute if num_days>=45.
+        :param start_date:
+        :param num_days:
+        :return:
+        """
+        for x in range(num_days):
+            the_day = start_date + timedelta(days=x)
+            weekday = the_day.weekday()
 
-        except HTTPError as e:
-            log.error("Request failed: %d %s", e.code, e.reason)
-        except URLError as e:
-            log.error("Server connection failed: %s", e.reason)
+            oncalls = self.get_on_call_engs(weekday)
+            if weekday < 6:
+                self.set_calendar_event(oncalls, the_day)
+
+    def set_calendar_event(self, users, start_date):
+        credentials = service_account.Credentials.from_service_account_info(
+            self.google_keyfile, scopes=['https://www.googleapis.com/auth/calendar'])
+
+        service = discovery.build('calendar', 'v3', credentials=credentials)
+
+        users_str = '/'.join([x.name for x in users])
+        desc_str = ""
+
+        weekday = start_date.weekday()
+
+        if weekday == 5:
+            end_date = start_date + timedelta(days=2)
+        else:
+            end_date = start_date + timedelta(days=1)
+
+        attendees = []
+        for user in users:
+            attendees.append({'email': user.value[0]})
+            desc_str += f'{user.name} ({user.value[0]}, {user.value[1]})\n'
+
+        event = {
+            "summary": f"On-Call {users_str}",
+            "location": "Earth",
+            "description": desc_str,
+            "start": {
+                "dateTime": start_date.strftime("%Y-%m-%dT00:00:00%z"),
+                "timeZone": settings.TIMEZONE
+            },
+            "end": {
+                "dateTime": end_date.strftime("%Y-%m-%dT00:00:00%z"),
+                "timeZone": settings.TIMEZONE
+            },
+            "attendees": attendees
+        }
+
+        service.events().insert(calendarId=self.google_calendar_id, body=event).execute()
 
     @property
     def slack_user_group(self):
@@ -87,71 +151,63 @@ class SlackOnCall(object):
                 self._slack_user_group = group
                 return group
 
-        raise ValueError('No user groups found that match {}'.format(self.slack_user_group_handle))
+        raise ValueError(f'No user groups found that match {self.slack_user_group_handle}')
 
-    @property
-    def on_call_email_addresses(self):
-        # todo rename oncall to in_rotation
+    def get_on_call_engs(self, weekday):
         """
-        Uses WEEKDAY_ROTATION on weekdays, otherwise uses the week number
+        Uses WEEKDAY_ROTA on weekdays, otherwise rotates the on-call in pairs
         based on the index stored in the last_oncall_index tag
         :return: All on-call email addresses
         """
-        if self._on_call_email_addresses is not None:
-            return self._on_call_email_addresses
+        users = set()
 
-        users = set()  # users can be in multiple schedule, this will de-dupe
+        if not settings.ROTATE_WEEKEND_ONCALL:
+            oncalls = settings.WEEKDAY_ROTATION[weekday]
+        elif weekday < 5:
+            oncalls = settings.WEEKDAY_ROTATION[weekday]
+        else:   # set a param that stores the index of the last on-call
+            eng_list = list(settings.People)
+            try:
+                oncall_index = int(SSM.get_parameter(Name='/oncall/last_oncall_index',
+                                                     WithDecryption=False)['Parameter']['Value'])
+            except ClientError:
+                oncall_index = 0
 
-        today = datetime.now(dateutil.tz.gettz(settings.TIMEZONE))
-        weekday = today.weekday()  # Monday is 0 and Sunday is 6
+            oncall_index = oncall_index % math.ceil(len(eng_list) / 2)
+            if weekday == 6:    # only increment once per weekend so we have the same pair
+                SSM.put_parameter(Name='/oncall/last_oncall_index',
+                                  Value=str(oncall_index + 1),
+                                  Overwrite=True,
+                                  Type='String')
 
-        if weekday < 5:
-            people_in_rotation = settings.WEEKDAY_ROTATION[weekday]
-        else:
-            week_num = today.isocalendar()[1]
-            people_in_rotation = settings.WEEKDAY_ROTATION[week_num % len(settings.WEEKDAY_ROTATION)]
+            oncalls = eng_list[oncall_index * 2:oncall_index * 2 + 2]
 
-            # todo what does this do again? this is some fancy stuff for weekends? turning it off for now...
-            # day_of_month = today.day
-            # eng_list = list(Engineers)
-            # client = boto3.client('lambda')
-            # tags = client.list_tags(
-            #     Resource=ARN_ID
-            # )
-            # oncall_index = int(tags['Tags']['last_oncall_index']) # int((day_of_month-1)/7)
-            # oncall_index = oncall_index % math.ceil(len(eng_list)/2)
-            # client.tag_resource(Resource=ARN_ID,
-            #             Tags={'last_oncall_index': str(oncall_index + 1)}
-            # )
-            #
-            # oncalls = eng_list[oncall_index*2:oncall_index*2+2]
-            
-        for peep in people_in_rotation:
-            users.add(peep.value)
+        for oncall in oncalls:
+            users.add(oncall)
 
-        log.info('Found %d users on-call', len(users))
-        self._on_call_email_addresses = users
+        log.info(f'Found {len(users)} users on-call')
+
         return users
 
     @property
     def all_slack_users(self):
-        # todo: make this a model we can use so we don't have to check the slack docs all the time
         if self._all_slack_users is not None:
             return self._all_slack_users
 
         url = 'https://slack.com/api/users.list'
         users = self._make_request(url)['members']
-        log.info('Found %d total Slack users', len(users))
+        log.info(f'Found {len(users)} total Slack users')
         self._all_slack_users = users
         return users
 
-    def slack_users_by_email(self, emails):
+    def slack_users_by_email(self, engs):
         """
         Finds all slack users by their email address
         :param emails: List of email address to find users
         :return: List of Slack user objects found in :emails:
         """
         users = []
+        emails = set([x.value[0] for x in engs])
         for user in self.all_slack_users:
             if user['profile'].get('email') in emails:
                 users.append(user)
@@ -165,18 +221,23 @@ class SlackOnCall(object):
         """
         user_ids = [u['id'] for u in slack_users]
         url = 'https://slack.com/api/usergroups.users.update?usergroup={0}&users={1}'.format(
-            self.slack_user_group['id'],
-            ','.join(user_ids)
-        )
+            self.slack_user_group['id'], ','.join(user_ids))
 
-        log.info('Updating user group %s from %s to %s',
-                 self.slack_user_group_handle, self.slack_user_group['users'], user_ids)
+        log.info(
+            'Updating user group {0} from {1} to {2}'.format(self.slack_user_group_handle,
+                                                             self.slack_user_group['users'],
+                                                             user_ids))
         self._make_request(url)
 
 
-def handler(event, context):
+def handler(_event, context):
+    """
+    Main entry point for AWS Lambda.
+    """
+    return SlackOnCall(slack_token=settings.SLACK_API_TOKEN,
+                       slack_user_group_handle=settings.ALIAS_NAME,
+                       arn=context.invoked_function_arn,
+                       keyfile=settings.GOOGLE_SERVICE_ACCOUNT_KEYFILE,
+                       gcal_id=settings.GOOGLE_CAL_ID,
+                       days_ahead=settings.DAYS_AHEAD).run()
 
-    return SlackOnCall(
-        slack_token=settings.SLACK_API_TOKEN,
-        slack_user_group_handle=settings.ALIAS_NAME
-    ).run()
--- settings.py
+++ settings.py
@@ -1,35 +1,29 @@
 import os
 from enum import unique, Enum
 
-ARN_ID = "arn:aws:lambda:us-west-1:<your lambda id>:function:update-slack-oncall"
 
 #TODO move these to a settings.yml so they can be re-used in the serverless yml
 # using https://serverless.com/framework/docs/providers/aws/guide/variables/#reference-variables-in-other-files
 
+STAGE = os.environ['stage']
 
 @unique
 class People(Enum):
     """
     <configure-me>
-    person1 = 'email1@email.com'
-    person2 = 'email2@email.com'
+    person1 = ('email1@email.com', phonenumber1)
+    person2 = ('email2@email.com', phonenumber2)
     """
 
 
 # order to mathc datetime.weekday()
 MON, TUE, WED, THU, FRI, SAT, SUN = range(7)
 
-
-# The Slack @user-group to update (Default: oncall)
-# If set to "oncall" it would be referenced via "@oncall" in slack
-ALIAS_NAME = "oncall"
-
-
 # A mapping of People to be set to the user group for each day
 # <configure-me>
 WEEKDAY_ROTATION = {
     # Monday
-    MON: [],  # [People.person1, People.person2],
+    MON: [],
     # Tuesday
     TUE: [],
     # Wednesday
@@ -40,11 +34,25 @@ WEEKDAY_ROTATION = {
     FRI: [],
 }
 
+# Rotates the oncall on weekends by pairs in the order of the People enum
+ROTATE_WEEKEND_ONCALL = True
+
+# The Slack @user-group to update (Default: oncall)
+# If set to "oncall" it would be referenced via "@oncall" in slack
+ALIAS_NAME = "oncall"
+
 # timezone you want to use for schedule. This will be used to determine what day it is for the WEEKDAY_ROTATION
-TIMEZONE = 'US/Pacific'
+TIMEZONE = 'America/Los_Angeles'
+
+# Number of days ahead to schedule calendar events
+DAYS_AHEAD = 45
+
+# Google Calendar ID (ex.- 'bloop.com_abcdef123456abc123@group.calendar.google.com'). Set to None to disable.
+GOOGLE_CAL_ID = None
 
 # sensitive vars loaded from env vars
 try:
     SLACK_API_TOKEN = os.environ['SLACK_SMART_ALIAS_SLACK_API_TOKEN']
+    GOOGLE_SERVICE_ACCOUNT_KEYFILE = os.environ['GOOGLE_SERVICE_ACCOUNT_KEYFILE']   # set to None to disable
 except KeyError as e:
     raise Exception("Make sure all the required env vars are set! {} was missing".format(e.args[0]))
